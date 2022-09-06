6206f9364357da62eedab4d35e8a7f40330c771f
yuvrajrajarawal869 <101393550+yuvrajrajarawal869@users.noreply.github.com>
2022-09-06 16:42:45 +0530

    first commit

---
+++ Script/transiction.js
@@ -0,0 +1,26 @@
+var menuScrollOffset = 0;
+var packageNUM = 1;
+var domain = '631385e4ac65c.site123.me';
+var languageCode = 'en';
+var multiLanCode = '';
+var globalLanguageChildLan          = '';
+var websiteLanguageCode             = '';
+var websiteLanguageCountryCode      = '';
+var websiteLanguageCountryFullCode  = '';
+var IsManagment                     = '1';
+var isMinimize                      = 1;
+var footerCustomTextJson = {"mailingList":{"placeHolder":"Enter your email","button":"Subscribe"},"cookieConsent":{"text":"This website uses cookies to ensure you get the best experience on our website","button":"Got it!"},"privacy":{"title":"Privacy"},"terms":{"title":"Terms"}};
+var websiteCustomSearchPlaceHolder = "Enter your query";
+
+var $GLOBALS = {
+    'cdn-user-files' : 'https://static.s123-cdn-static-d.com',
+    'maps-display-domain' : 'https://maps-cdn.site123.com',
+    'cdn-system-files' : 'https://cdn-cms-s.f-static.net',
+    'cdn-images-files' : 'https://static1.s123-cdn-static-a.com',
+    'v-cache' : 'r82140',
+    'is_local_server' : false        };
+var longFreeCustomer = false;
+        // set progressive web applications settings object for published websites only
+var pwaSettings = {
+    enableMessage: false,
+};
